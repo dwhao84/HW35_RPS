36a525e8541e377159f94fbe1d9cf9c9c5d7f20c
Daweihao84 <dwsamurai84@gmail.com>
2023-10-07 22:25:09 +0800

    Initial Commit

---
+++ HW35_RPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW35_RPS
+//
+//  Created by Dawei Hao on 2023/10/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
